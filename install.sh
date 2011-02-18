dd87dce71374cc8196db2cbe8275f48baf76e2d2
Sascha Szott <szott@gmx.de>
2011-02-18 19:14:48 +0000

    BUGFIX: added new install script for OPUS installation under SuSE Linux
    git-svn-id: https://svn.zib.de/opus4dev/install@7609 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # LICENCE
 # This code is free software: you can redistribute it and/or modify
@@ -12,14 +12,32 @@
 # GNU General Public License for more details.
 #
 # @author      Sascha Szott <szott@zib.de>
-# @copyright   Copyright (c) 2010, OPUS 4 development team
+# @copyright   Copyright (c) 2010-2011, OPUS 4 development team
 # @license     http://www.gnu.org/licenses/gpl.html General Public License
 # @version     $Id$
 
-#set -ex
 set -e
 
+SCRIPT_NAME=`basename $0`
+
+if [ $# -lt 1 ]
+then
+  echo "Missing Argument: use $SCRIPT_NAME {ubuntu,suse}"
+  echo "Installation aborted."
+  exit 1
+fi
+
+if [ $1 = "ubuntu" -o $1 = "suse" ]
+then
+  OS=$1
+else
+  echo "Invalid Argument $1 : use $SCRIPT_NAME {ubuntu,suse}"
+  echo "Installation aborted."
+  exit 1 
+fi
+
 BASEDIR=/var/local/opus4
+MYSQL_CLIENT=/usr/bin/mysql
 
 ZEND_LIB_URL='http://framework.zend.com/releases/ZendFramework-1.10.6/ZendFramework-1.10.6-minimal.tar.gz'
 JPGRAPH_LIB_URL='http://jpgraph.net/download/download.php?p=1'
@@ -27,8 +45,6 @@ SOLR_SERVER_URL='http://www.apache.org/dist//lucene/solr/1.4.1/apache-solr-1.4.1
 SOLR_PHP_CLIENT_LIB_URL='http://solr-php-client.googlecode.com/svn/trunk/'
 SOLR_PHP_CLIENT_LIB_REVISION=36
 JQUERY_LIB_URL='http://code.jquery.com/jquery-1.4.3.min.js'
-
-MYSQL_CLIENT=/usr/bin/mysql
 
 cd $BASEDIR
 
@@ -96,7 +112,14 @@ read -p "System Account Name [opus4]: " OPUS_USER_NAME
 if [ -z "$OPUS_USER_NAME" ]; then
   OPUS_USER_NAME=opus4
 fi
-useradd -c 'OPUS 4 Solr manager' --system $OPUS_USER_NAME
+OPUS_USER_NAME_ESC=`echo "$OPUS_USER_NAME" | sed 's/\!/\\\!/g'`
+
+if [ $OS = "ubuntu" ]
+then
+  useradd -c 'OPUS 4 Solr manager' --system $OPUS_USER_NAME
+else
+  useradd -c 'OPUS 4 Solr manager' --system --create-home --shell /bin/bash $OPUS_USER_NAME
+fi
 
 # prompt for database parameters
 read -p "New OPUS Database Name [opus400]: "          DBNAME
@@ -132,6 +155,15 @@ else
   HOST="$MYSQLHOST"
 fi
 
+# escape ! (for later use in sed substitute)
+MYSQLHOST_ESC=`echo "$MYSQLHOST" | sed 's/\!/\\\!/g'`
+MYSQLPORT_ESC=`echo "$MYSQLPORT" | sed 's/\!/\\\!/g'`
+WEBAPP_USER_ESC=`echo "$WEBAPP_USER" | sed 's/\!/\\\!/g'`
+WEBAPP_USER_PASSWORD_ESC=`echo "$WEBAPP_USER_PASSWORD" | sed 's/\!/\\\!/g'`
+DBNAME_ESC=`echo "$DBNAME" | sed 's/\!/\\\!/g'`
+ADMIN_ESC=`echo "$ADMIN" | sed 's/\!/\\\!/g'`
+ADMIN_PASSWORD_ESC=`echo "$ADMIN_PASSWORD" | sed 's/\!/\\\!/g'`
+
 # process creating mysql user and database
 MYSQL="$MYSQL_CLIENT --default-character-set=utf8 -u $MYSQLROOT -p -v"
 MYSQL_OPUS4ADMIN="$MYSQL_CLIENT --default-character-set=utf8 -u $ADMIN -p$ADMIN_PASSWORD -v"
@@ -156,19 +188,19 @@ LimitString
 
 # create config.ini and set database related parameters
 cd $BASEDIR/opus4/application/configs
-sed -e "s!^db.params.host =!db.params.host ='$MYSQLHOST'!" \
-    -e "s!^db.params.port =!db.params.port = '$MYSQLPORT'!" \
-    -e "s!^db.params.username =!db.params.username = '$WEBAPP_USER'!" \
-    -e "s!^db.params.password =!db.params.password = '$WEBAPP_USER_PASSWORD'!" \
-    -e "s!^db.params.dbname =!db.params.dbname = '$DBNAME'!" config.ini.template > config.ini
+sed -e "s!^db.params.host =!db.params.host = '$MYSQLHOST_ESC'!" \
+    -e "s!^db.params.port =!db.params.port = '$MYSQLPORT_ESC'!" \
+    -e "s!^db.params.username =!db.params.username = '$WEBAPP_USER_ESC'!" \
+    -e "s!^db.params.password =!db.params.password = '$WEBAPP_USER_PASSWORD_ESC'!" \
+    -e "s!^db.params.dbname =!db.params.dbname = '$DBNAME_ESC'!" config.ini.template > config.ini
 
 # create createdb.sh and set database related parameters
 cd $BASEDIR/opus4/db
-sed -e "s!^user=!user='$ADMIN'!" \
-    -e "s!^password=!password='$ADMIN_PASSWORD'!" \
-    -e "s!^host=!host='$MYSQLHOST'!" \
-    -e "s!^port=!port='$MYSQLPORT'!" \
-    -e "s!^dbname=!dbname='$DBNAME'!" createdb.sh.template > createdb.sh
+sed -e "s!^user=!user='$ADMIN_ESC'!" \
+    -e "s!^password=!password='$ADMIN_PASSWORD_ESC'!" \
+    -e "s!^host=!host='$MYSQLHOST_ESC'!" \
+    -e "s!^port=!port='$MYSQLPORT_ESC'!" \
+    -e "s!^dbname=!dbname='$DBNAME_ESC'!" createdb.sh.template > createdb.sh
 chmod +x createdb.sh
 ./createdb.sh
 
@@ -193,21 +225,27 @@ then
   if [ -z "$SOLR_SERVER_PORT" ]; then
     SOLR_SERVER_PORT=8983;
   fi
+  SOLR_SERVER_PORT_ESC=`echo "$SOLR_SERVER_PORT" |sed 's/\!/\\\!/g'`
+
   cd $BASEDIR/opus4/application/configs
   cp config.ini config.ini.tmp
-  sed -e "s!^searchengine.index.host =!searchengine.index.host = '$HOST'!" \
-      -e "s!^searchengine.index.port =!searchengine.index.port = '$SOLR_SERVER_PORT'!" \
+  sed -e "s!^searchengine.index.host =!searchengine.index.host = 'localhost'!" \
+      -e "s!^searchengine.index.port =!searchengine.index.port = '$SOLR_SERVER_PORT_ESC'!" \
       -e "s!^searchengine.index.app =!searchengine.index.app = 'solr'!" \
-      -e "s!^searchengine.extract.host =!searchengine.extract.host = '$HOST'!" \
-      -e "s!^searchengine.extract.port =!searchengine.extract.port = '$SOLR_SERVER_PORT'!" \
+      -e "s!^searchengine.extract.host =!searchengine.extract.host = 'localhost'!" \
+      -e "s!^searchengine.extract.port =!searchengine.extract.port = '$SOLR_SERVER_PORT_ESC'!" \
       -e "s!^searchengine.extract.app =!searchengine.extract.app = 'solr'!" config.ini.tmp > config.ini 
   rm config.ini.tmp
 
   cd $BASEDIR/install
-  mv opus4-solr-jetty.conf opus4-solr-jetty.conf.tmp
-  sed -e "s!^JETTY_PORT=!JETTY_PORT=$SOLR_SERVER_PORT!" \
-      -e "s!^JETTY_USER=!JETTY_USER=$OPUS_USER_NAME!" opus4-solr-jetty.conf.tmp > opus4-solr-jetty.conf
-  rm opus4-solr-jetty.conf.tmp
+  if [ $OS = "suse" ]
+  then
+    cp opus4-solr-jetty opus4-solr-jetty.tmp
+    sed -e "s!^START_STOP_DAEMON=1!START_STOP_DAEMON=0!" opus4-solr-jetty.tmp > opus4-solr-jetty
+    rm opus4-solr-jetty.tmp
+  fi
+  sed -e "s!^JETTY_PORT=!JETTY_PORT=$SOLR_SERVER_PORT_ESC!" \
+      -e "s!^JETTY_USER=!JETTY_USER=$OPUS_USER_NAME_ESC!" opus4-solr-jetty.conf.template > opus4-solr-jetty.conf
   chmod +x opus4-solr-jetty
 
   read -p "Install init.d script to start and stop Solr server automatically? [Y]: " INSTALL_INIT_SCRIPT
@@ -216,13 +254,25 @@ then
     ln -sf $BASEDIR/install/opus4-solr-jetty /etc/init.d/opus4-solr-jetty
     ln -sf $BASEDIR/install/opus4-solr-jetty.conf /etc/default/jetty
     chmod +x /etc/init.d/opus4-solr-jetty
-    update-rc.d -f opus4-solr-jetty remove
-    update-rc.d opus4-solr-jetty defaults
+    if [ $OS = "ubuntu" ]
+    then
+      update-rc.d -f opus4-solr-jetty remove
+      update-rc.d opus4-solr-jetty defaults
+    else
+      chkconfig --del opus4-solr-jetty
+      chkconfig --set opus4-solr-jetty on
+    fi
   fi
 
   # change file owner of solr installation
-  chown -R $OPUS_USER_NAME:$OPUS_USER_NAME $BASEDIR/apache-solr-1.4.1
-  chown -R $OPUS_USER_NAME:$OPUS_USER_NAME $BASEDIR/solrconfig
+  if [ $OS = "ubuntu" ]
+  then
+    OWNER="$OPUS_USER_NAME:$OPUS_USER_NAME"
+  else
+    OWNER="$OPUS_USER_NAME"
+  fi
+  chown -R $OWNER $BASEDIR/apache-solr-1.4.1
+  chown -R $OWNER $BASEDIR/solrconfig
 
   # start Solr server
   ./opus4-solr-jetty start
@@ -252,15 +302,19 @@ then
   done
 
   echo "completed."
-  echo "Solr server is running under http://localhost:$SOLR_SERVER_PORT/solr"
-  echo ""
+  echo -e "Solr server is running under http://localhost:$SOLR_SERVER_PORT/solr\n"
 
   # start indexing of testdata
   php5 $BASEDIR/opus4/scripts/SolrIndexBuilder.php
 fi
 
 # change file owner to $OPUS_USER_NAME
-chown -R $OPUS_USER_NAME:$OPUS_USER_NAME $BASEDIR
+if [ $OS = "ubuntu" ]
+then
+  chown -R $OPUS_USER_NAME:$OPUS_USER_NAME $BASEDIR
+else
+  chown -R $OPUS_USER_NAME $BASEDIR
+fi
 cd $BASEDIR/workspace
 chmod -R 777 *
 
